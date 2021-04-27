e3b38f7e4a41bc4f8c1e7f3d58e363f45cc488d6
Zachary Eggert <Eggert.Zachary@gmail.com>
2021-04-27 16:32:53 -0700

    Add Category Routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,26 +4,66 @@ const sequelize = require('../../config/connection')
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try{
+    const result = await Category.findAll({include: Product});
+    res.status(200).json(result);
+  }catch(err){
+    console.error(err);
+    res.status(500).json(err);
+  }
+
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try{
+    const result = await Category.findByPk(req.params.id, {include: Product});
+    res.status(200).json(result);
+  }catch(err){
+    console.error(err);
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try{
+    const result = await Category.create(req.body);
+    res.status(200).json(result);
+  }catch(err){
+    console.error(err);
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try{
+    const result = await Category.update(req.body, {
+      where: {id: req.params.id}
+    });
+    res.status(200).json(result);
+  }catch(err){
+    console.error(err);
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try{
+    const result = await Category.destroy({
+      where: {id: req.params.id}
+    });
+    res.status(200).json(result);
+  }catch(err){
+    console.error(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- routes/api/index.js
+++ routes/api/index.js
@@ -2,9 +2,15 @@ const router = require('express').Router();
 const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
+const seed = require('../../seeds/index.js')
 
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
+
+router.post('/seed', (req, res) => {
+    seed.seedAll();
+    res.status(200).json({message: 'Successfully Seeded'})
+})
 
 module.exports = router;
